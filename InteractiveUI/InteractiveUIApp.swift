8e5d58d031d118f3a0739fa51a0494e327353b53
Grace Shen <graceshen6439@gmaiil.com>
2025-07-09 13:14:51 -0400

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Grace Shen on 2025-07-09.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveUITests/InteractiveUITests.swift
@@ -0,0 +1,16 @@
+//
+//  InteractiveUITests.swift
+//  InteractiveUITests
+//
+//  Created by Grace Shen on 2025-07-09.
+//
+
+import Testing
+
+struct InteractiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
